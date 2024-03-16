1474f8ef09bacfe523708009f01032f4277a7318
Rohan <139254497+rohanramidwar@users.noreply.github.com>
2024-03-17 00:46:46 +0530

    slots working

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -1,8 +1,9 @@
-import express from 'express';
-import {createSchedule} from '../controllers/schedule.js';
+import express from "express";
+import { createSchedule } from "../controllers/schedule.js";
+import { createSlots } from "../controllers/interviewSlotController.js";
 
 const router = express.Router();
 
-router.post('/createSchedule', createSchedule);
+router.post("/createSchedule", createSlots);
 
 export default router;
