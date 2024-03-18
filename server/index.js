337e87c308b546e7c15113581655365ca4d35917
Ryan <ryan.work321@gmail.com>
2024-03-19 01:17:13 +0530

    email shortlisted candidates

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ import createSchedule from './routes/schedule.js'
 import candidateJob from './routes/candidateJob.js';
 import Stripe from 'stripe';
 import Profile from './routes/Profile.js';
+import sendEmails from './routes/sendEmails.js';
 
 dotenv.config();
 const stripe = Stripe(process.env.SECRET_KEY);
@@ -69,8 +70,8 @@ app.post("/checkout", async(req, res) => {
 });
 
 app.use('/profile', Profile);
-
 app.use('/ai-interview',AIInterview);
+app.use('/send-emails', sendEmails);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port : ${PORT}`);
